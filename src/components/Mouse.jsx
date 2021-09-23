c0803595bdbb400a6ee59c971edf5079bdc5bf1a
unknown <stnqls3938@gmail.com>
2021-09-23 14:45:51 +0900

    Mouse.jsx class형 컴포넌트 주석 삭제

--- src/components/Mouse.jsx
+++ src/components/Mouse.jsx
@@ -1,36 +1,5 @@
 import React, { useState } from "react";
 
-// class Mouse extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.handleMouseMove = this.handleMouseMove.bind(this);
-//     this.state = { x: 0, y: 0 };
-//   }
-
-//   handleMouseMove(e) {
-//     this.setState({
-//       x: e.clientX,
-//       y: e.clientY,
-//     });
-//   }
-
-//   render() {
-//     return (
-//       <div
-//         style={{
-//           height: "600px",
-//           width: "600px",
-//           backgroundColor: "lightblue",
-//         }}
-//         onMouseMove={this.handleMouseMove}
-//       >
-//         {this.props.render(this.state)}
-//       </div>
-//     );
-//   }
-// }
-// export default Mouse;
-
 export default function Mouse(props) {
   const [move, setMove] = useState(0);
   function handleMouseMove(e) {
@@ -48,7 +17,6 @@ export default function Mouse(props) {
       }}
       onMouseMove={handleMouseMove}
     >
-      {/* {this.props.render(move)} */}
       {props.render(move)}
     </div>
   );
