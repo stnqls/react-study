9d7a69fd2f6921a1d7430c5051650b32fc99d55b
unknown <stnqls3938@gmail.com>
2021-09-23 14:23:13 +0900

    Mouse 함수형 컴포넌트로 바꾸기

--- src/App.js
+++ src/App.js
@@ -2,26 +2,26 @@
 // import Example2 from "./components/basicHooks/Example2";
 // import Example3 from "./components/basicHooks/Example3";
 // import Example4 from "./components/basicHooks/Example4";
-// import Example5 from "./components/basicHooks/Example5";
-import Counter from "./components/basicHooks/Example";
-import useWindowWidth from "./hooks/useWindowWidth";
+import Example5 from "./components/basicHooks/Example5";
+// import Counter from "./components/basicHooks/Example";
+// import useWindowWidth from "./hooks/useWindowWidth";
 import withHasMounted from "./hocs/withHasMounted";
-import useHasMounted from "./hooks/useHasMounted";
+// import useHasMounted from "./hooks/useHasMounted";
 import MouseTracker from "./components/MouseTracker";
 
 function App({ hasMounted }) {
-  const width = useWindowWidth();
-  const hasMountedFromHooks = useHasMounted();
-  console.log(hasMounted, hasMountedFromHooks);
+  // const width = useWindowWidth();
+  // const hasMountedFromHooks = useHasMounted();
+  // console.log(hasMounted, hasMountedFromHooks);
   return (
     <div className="App">
       {/* <Example1 />
       <Example2 />
       <Example3 />
-      <Example4 />
-      <Example5 /> */}
-      <Counter />
-      {width}
+      <Example4 /> */}
+      <Example5 />
+      {/* <Counter />
+      {width} */}
       <MouseTracker />
     </div>
   );
--- src/components/Mouse.jsx
+++ src/components/Mouse.jsx
@@ -1,33 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 
-class Mouse extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleMouseMove = this.handleMouseMove.bind(this);
-    this.state = { x: 0, y: 0 };
-  }
+// class Mouse extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.handleMouseMove = this.handleMouseMove.bind(this);
+//     this.state = { x: 0, y: 0 };
+//   }
 
-  handleMouseMove(e) {
-    this.setState({
+//   handleMouseMove(e) {
+//     this.setState({
+//       x: e.clientX,
+//       y: e.clientY,
+//     });
+//   }
+
+//   render() {
+//     return (
+//       <div
+//         style={{
+//           height: "600px",
+//           width: "600px",
+//           backgroundColor: "lightblue",
+//         }}
+//         onMouseMove={this.handleMouseMove}
+//       >
+//         {this.props.render(this.state)}
+//       </div>
+//     );
+//   }
+// }
+// export default Mouse;
+
+export default function Mouse(props) {
+  const [move, setMove] = useState(0);
+  function handleMouseMove(e) {
+    setMove({
       x: e.clientX,
       y: e.clientY,
     });
   }
-
-  render() {
-    return (
-      <div
-        style={{
-          height: "600px",
-          width: "600px",
-          backgroundColor: "lightblue",
-        }}
-        onMouseMove={this.handleMouseMove}
-      >
-        {this.props.render(this.state)}
-      </div>
-    );
-  }
+  return (
+    <div
+      style={{
+        height: "600px",
+        width: "600px",
+        backgroundColor: "lightblue",
+      }}
+      onMouseMove={handleMouseMove}
+    >
+      {/* {this.props.render(move)} */}
+      {props.render(move)}
+    </div>
+  );
 }
-
-export default Mouse;
