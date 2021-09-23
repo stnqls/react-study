ee02b2d368bece4274592165dd1bfe068ea5ed8a
unknown <stnqls3938@gmail.com>
2021-09-23 11:17:37 +0900

    Mouse로 캡슐화

---
+++ src/components/Mouse.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+class Mouse extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleMouseMove = this.handleMouseMove.bind(this);
+    this.state = { x: 0, y: 0 };
+  }
+
+  handleMouseMove(e) {
+    this.setState({
+      x: e.clientX,
+      y: e.clientY,
+    });
+  }
+
+  render() {
+    return (
+      <div
+        style={{
+          height: "600px",
+          width: "600px",
+          backgroundColor: "lightblue",
+        }}
+        onMouseMove={this.handleMouseMove}
+      >
+        <p>
+          The current mouse position is ({this.state.x}, {this.state.y})
+        </p>
+      </div>
+    );
+  }
+}
+
+export default Mouse;
--- src/components/MouseTracker.jsx
+++ src/components/MouseTracker.jsx
@@ -1,33 +1,13 @@
 import React from "react";
+import Mouse from "./Mouse";
 
 class MouseTracker extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleMouseMove = this.handleMouseMove.bind(this);
-    this.state = { x: 0, y: 0 };
-  }
-
-  handleMouseMove(e) {
-    this.setState({
-      x: e.clientX,
-      y: e.clientY,
-    });
-  }
   render() {
     return (
-      <div
-        style={{
-          height: "600px",
-          width: "600px",
-          backgroundColor: "lightblue",
-        }}
-        onMouseMove={this.handleMouseMove}
-      >
+      <React.Fragment>
         <h1>Move the mouse around!</h1>
-        <p>
-          The current mouse position is ({this.state.x}, {this.state.y})
-        </p>
-      </div>
+        <Mouse />
+      </React.Fragment>
     );
   }
 }
