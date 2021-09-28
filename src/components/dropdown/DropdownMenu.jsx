03bd688ec93de1737112d5cfa0428f523f77d29c
unknown <stnqls3938@gmail.com>
2021-09-28 12:07:12 +0900

    DropdownMenu - useState,useEffect추출

--- src/components/dropdown/DropdownMenu.jsx
+++ src/components/dropdown/DropdownMenu.jsx
@@ -1,27 +1,11 @@
-import { useEffect, useRef, useState } from "react";
+import { useRef } from "react";
 import "./DropdownMenu.css";
+import { useDetectOutsideClick } from "./useDetectOutsideClick";
 
 const DropdownMenu = () => {
   const dropdownRef = useRef(null);
-  const [isActive, setIsActive] = useState(false);
+  const [isActive, setIsActive] = useDetectOutsideClick(dropdownRef, false);
   const onClick = () => setIsActive(!isActive);
-
-  useEffect(() => {
-    const pageClickEvent = (e) => {
-      if (
-        dropdownRef.current !== null &&
-        !dropdownRef.current.contains(e.target)
-      ) {
-        setIsActive(!isActive);
-      }
-    };
-    if (isActive) {
-      window.addEventListener("click", pageClickEvent);
-    }
-    return () => {
-      window.removeEventListener("click", pageClickEvent);
-    };
-  }, [isActive]);
 
   return (
     <div className="menu-container">
