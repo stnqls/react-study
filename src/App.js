53ad549123920d8c36312ac989aad03496365276
unknown <stnqls3938@gmail.com>
2021-10-27 18:42:29 +0900

    전체

--- src/App.js
+++ src/App.js
@@ -4,50 +4,52 @@
 // import Example4 from "./components/basicHooks/Example4";
 // import Example5 from "./components/basicHooks/Example5";
 // import Counter from "./components/basicHooks/Example";
-import useWindowWidth from "./hooks/useWindowWidth";
-import withHasMounted from "./hocs/withHasMounted";
+// import useWindowWidth from "./hooks/useWindowWidth";
+// import withHasMounted from "./hocs/withHasMounted";
 // import useHasMounted from "./hooks/useHasMounted";
-import MouseTracker from "./components/MouseTracker";
-import CustomTextInput from "./components/CustomTextInput";
+// import MouseTracker from "./components/MouseTracker";
+// import CustomTextInput from "./components/CustomTextInput";
 // import AutoFocusTextInput from "./components/AutoFocusTextInput";
-import ValidationSample from "./components/ValidationSample";
-import ScrollBoxContainer from "./components/ScrollBoxContainer";
+// import ValidationSample from "./components/ValidationSample";
+// import ScrollBoxContainer from "./components/ScrollBoxContainer";
 
-import Example from "./components/carousel/Example";
+// import Example from "./components/carousel/Example";
 // import ButtonWithDropDown from "./components/dropdown/ButtonWithDropDown";
-import DropdownMenu from "./components/dropdown/DropdownMenu";
-import Dropdown from "./components/dropdown/Dropdown";
-import FooterSaveButton from "./FooterSaveButton";
-import SaveButton from "./components/SaveButton";
+// import DropdownMenu from "./components/dropdown/DropdownMenu";
+// import Dropdown from "./components/dropdown/Dropdown";
+// import FooterSaveButton from "./FooterSaveButton";
+// import SaveButton from "./components/SaveButton";
+
+// import PostMain from "./components/post/PostMain";
 
 function App({ hasMounted }) {
-  const width = useWindowWidth();
+  // const width = useWindowWidth();
   // const hasMountedFromHooks = useHasMounted();
   // console.log(hasMounted, hasMountedFromHooks);
   return (
     <div className="App">
-      {/* <Example1 /> */}
-      {/* <Example2 /> */}
-      {/* <Example3 /> */}
-      {/* <Example4 /> */}
-      {/* <Example5 /> */}
-      {/* <Counter /> */}
+      {/* <Example1 />
+      <Example2 />
+      <Example3 />
+      <Example4 />
+      <Example5 />
+      <Counter />
       {width}
       <MouseTracker />
       <CustomTextInput />
-      {/* <AutoFocusTextInput /> */}
+      <AutoFocusTextInput />
       <ValidationSample />
       <ScrollBoxContainer />
       <Example />
-      {/* <ButtonWithDropDown /> */}
+      <ButtonWithDropDown />
 
-      {/* function */}
       <DropdownMenu />
-      {/* class */}
       <Dropdown />
       <FooterSaveButton />
       <SaveButton />
+
+      <PostMain /> */}
     </div>
   );
 }
-export default withHasMounted(App);
+export default App;
---
+++ src/components/post/PostMain.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import PostList from "./PostList";
+
+const PostMain = () => {
+  return (
+    <>
+      <PostList />
+    </>
+  );
+};
+
+export default PostMain;
