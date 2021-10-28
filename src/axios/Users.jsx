87309cd049b486df73ccf36d26ae13544fd3ca4a
unknown <stnqls3938@gmail.com>
2021-10-28 11:53:03 +0900

    axios사용하기 - 버튼추가

--- src/axios/Users.jsx
+++ src/axios/Users.jsx
@@ -5,25 +5,24 @@ function Users() {
   const [users, setUsers] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
+  const fetchUsers = async () => {
+    try {
+      //요청이 시잘 할 때
+      setError(null);
+      setUsers(null);
+      setLoading(true);
 
+      const response = await axios.get(
+        "https://jsonplaceholder.typicode.com/users"
+      );
+      // axios.get() : 데이터 조회
+      setUsers(response.data);
+    } catch (e) {
+      setError(e);
+    }
+    setLoading(false);
+  };
   useEffect(() => {
-    const fetchUsers = async () => {
-      try {
-        //요청이 시잘 할 때
-        setError(null);
-        setUsers(null);
-        setLoading(true);
-
-        const response = await axios.get(
-          "https://jsonplaceholder.typicode.com/users"
-        );
-        // axios.get() : 데이터 조회
-        setUsers(response.data);
-      } catch (e) {
-        setError(e);
-      }
-      setLoading(false);
-    };
     fetchUsers();
   }, []);
 
@@ -31,13 +30,16 @@ function Users() {
   if (error) return <div>에러가 발생했습니다.</div>;
   if (!users) return null;
   return (
-    <ul>
-      {users.map((user) => (
-        <li key={user.id}>
-          {user.username} ({user.name})
-        </li>
-      ))}
-    </ul>
+    <>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            {user.username} ({user.name})
+          </li>
+        ))}
+      </ul>
+      <button onClick={fetchUsers}>다시 불러오기</button>
+    </>
   );
 }
 
