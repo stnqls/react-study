b90688ef5a55a5ffe6fccc4f23f4a81f68706895
unknown <stnqls3938@gmail.com>
2021-10-28 11:49:18 +0900

    axios 사용하기

--- src/App.js
+++ src/App.js
@@ -1,54 +1,9 @@
-// import Example1 from "./components/basicHooks/Example1";
-// import Example2 from "./components/basicHooks/Example2";
-// import Example3 from "./components/basicHooks/Example3";
-// import Example4 from "./components/basicHooks/Example4";
-// import Example5 from "./components/basicHooks/Example5";
-// import Counter from "./components/basicHooks/Example";
-// import useWindowWidth from "./hooks/useWindowWidth";
-// import withHasMounted from "./hocs/withHasMounted";
-// import useHasMounted from "./hooks/useHasMounted";
-// import MouseTracker from "./components/MouseTracker";
-// import CustomTextInput from "./components/CustomTextInput";
-// import AutoFocusTextInput from "./components/AutoFocusTextInput";
-// import ValidationSample from "./components/ValidationSample";
-// import ScrollBoxContainer from "./components/ScrollBoxContainer";
+import Users from "./axios/Users";
 
-// import Example from "./components/carousel/Example";
-// import ButtonWithDropDown from "./components/dropdown/ButtonWithDropDown";
-// import DropdownMenu from "./components/dropdown/DropdownMenu";
-// import Dropdown from "./components/dropdown/Dropdown";
-// import FooterSaveButton from "./FooterSaveButton";
-// import SaveButton from "./components/SaveButton";
-
-// import PostMain from "./components/post/PostMain";
-
-function App({ hasMounted }) {
-  // const width = useWindowWidth();
-  // const hasMountedFromHooks = useHasMounted();
-  // console.log(hasMounted, hasMountedFromHooks);
+function App() {
   return (
     <div className="App">
-      {/* <Example1 />
-      <Example2 />
-      <Example3 />
-      <Example4 />
-      <Example5 />
-      <Counter />
-      {width}
-      <MouseTracker />
-      <CustomTextInput />
-      <AutoFocusTextInput />
-      <ValidationSample />
-      <ScrollBoxContainer />
-      <Example />
-      <ButtonWithDropDown />
-
-      <DropdownMenu />
-      <Dropdown />
-      <FooterSaveButton />
-      <SaveButton />
-
-      <PostMain /> */}
+      <Users />
     </div>
   );
 }
---
+++ src/axios/Users.jsx
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+function Users() {
+  const [users, setUsers] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        //요청이 시잘 할 때
+        setError(null);
+        setUsers(null);
+        setLoading(true);
+
+        const response = await axios.get(
+          "https://jsonplaceholder.typicode.com/users"
+        );
+        // axios.get() : 데이터 조회
+        setUsers(response.data);
+      } catch (e) {
+        setError(e);
+      }
+      setLoading(false);
+    };
+    fetchUsers();
+  }, []);
+
+  if (loading) return <div>로딩중...</div>;
+  if (error) return <div>에러가 발생했습니다.</div>;
+  if (!users) return null;
+  return (
+    <ul>
+      {users.map((user) => (
+        <li key={user.id}>
+          {user.username} ({user.name})
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default Users;
