e3b3fa1e77de76f757d331f2ccbb1c2251135d4b
seonsubin <stnqls3938@gmail.com>
2021-11-03 11:03:06 +0900

    Return Map

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Fragments from "./book/part2/Fragments";
 import ReactHook from "./book/part2/ReactHook";
+import ReturnMap from "./book/part2/ReturnMap";
 
 function App() {
   return (
     <div className="App">
       <ReactHook />
       <Fragments />
+      <ReturnMap />
     </div>
   );
 }
---
+++ src/book/part2/ReturnMap.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+class ReturnMap extends React.Component {
+  render() {
+    const element_Array = [<li>react</li>, <li>200</li>, <li>Array map</li>];
+    return <ul>{element_Array.map((array_val) => array_val)}</ul>;
+  }
+}
+
+export default ReturnMap;
